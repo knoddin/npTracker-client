6f149abf1dbe5fdff76c25bd0f27931e8b8dd3a1
knoddin <knoddin@gmail.com>
2016-11-07 14:59:54 -0500

    changed environment

--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,7 @@ module.exports = function (environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    apiHost: 'http://localhost:4741/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -49,7 +50,10 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
+    ENV.baseURL ='/';
     ENV.locationType = 'hash';
+    ENV.apiHost = 'https://blooming-beach-29358.herokuapp.com/'
+
   }
 
   return ENV;
