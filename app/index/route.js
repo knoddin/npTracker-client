2324cca779a68f14e39d5266b808c27ba49b0a65
knoddin <knoddin@gmail.com>
2016-11-10 00:42:56 -0500

    Abandon fancy transition backgrounds -- @jrhorn424

--- app/index/route.js
+++ app/index/route.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  activate: function(){
-    Ember.$('body').css('background', 'url("/assets/images/np.jpeg") no-repeat center center fixed');
-    Ember.$('body').css('background-size', 'cover');
-  },
-
-  // when router exits the route
-  deactivate: function(){
-    Ember.$('body').css('background', "none");
-  },
 });
